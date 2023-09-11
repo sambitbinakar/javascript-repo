29a5d1323334775356fbc2d7cd4a7eaf770d628a
sambit binakar <sambitbinakar@gmail.com>
2023-09-11 20:10:14 +0530

    complete variables

---
+++ variables.js
@@ -0,0 +1,28 @@
+const accountid=144553
+let accountemail = 'sambi@gmail.com'
+var password = 878799878798
+city ='bhubaneswar';
+
+
+accountemail = 'sb@gamil.com'
+password =121212122
+city ="puri"
+/*
+prefer not to use var
+because  of issue in block scope and function scope
+
+var x = 2;     // Allowed
+const x = 2;   // Not allowed
+
+{
+let x = 2;     // Allowed
+const x = 2;   // Not allowed
+}
+
+{
+const x = 2;   // Allowed
+const x = 2;   // Not allowed
+}
+*/
+console.log(accountid);
+console.table([accountid ,accountemail , password])

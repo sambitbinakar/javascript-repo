2a59ae1436d10b98b2478263d6a1f50bf0ef8dff
sambit binakar <sambitbinakar@gmail.com>
2023-09-17 22:46:32 +0530

    operationsconfig

---
+++ operations.js
@@ -0,0 +1,37 @@
+let str1 ="hello"
+let str2 =" sambit"
+let str3 = str1+str2
+console.log (str3)
+
+console.log ("1" +2); //12
+console.log(1+"2"); //12
+console.log("1" +2+2);  //122
+console.log (2+2+"2");  //42
+console.log(true) //true
+console.log(+true)  //1
+console.log(+"")  // 0
+
+let num =100;
+++ num;
+console.log(num); //101 
+
+//   comparision  //
+
+console.log (1 > 3);
+console.log (2 >= 4);
+console.log (3 < 4);
+console.log (4 <= 5);
+
+console.log ("2"> 1);
+
+
+console.log (null >0);
+console.log (null == 0);
+console.log (null >= 0);
+
+console.log ( undefined == 0);
+console.log (undefined > 0);
+console.log (undefined < 0);
+////we avoid  the null  and undefined value comparision  for clean code 
+
+console.log( "2" === 2 );   /// check  the datatype  if datatype same they  check
